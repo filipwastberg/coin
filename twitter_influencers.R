124c60ed630fc59ac12fb2c2e85c8d1429cd612e
filipwastberg <filip.wastberg@gmail.com>
2018-02-06 17:52:33 +0100

    twitter updated

--- twitter_influencers.R
+++ twitter_influencers.R
@@ -1,6 +1,7 @@
 # Scrape Twitter influencers
 url <- "https://www.marketwatch.com/story/bitcoin-and-cryptocurrency-on-twitter-the-most-important-people-to-follow-2017-12-04"
 
+# Scrape website
 library(rvest)
 influencers <- read_html(url) %>% 
     html_nodes('p') %>% 
@@ -8,6 +9,7 @@ influencers <- read_html(url) %>%
 
 influencers <- influencers[11:72]
 
+# Clean text and extract twitter names
 library(stringr)
 infl_twitter <- influencers %>%
   str_extract_all("@[[:alpha:][:punct:]]*") %>%
@@ -16,3 +18,13 @@ infl_twitter <- influencers %>%
   rename(influencer = value) %>%
   mutate(influencer = str_replace(influencer, "[[:punct:]]$" , "")) %>%
   filter(influencer != "")
+
+#library(readr)
+#write_csv(infl_twitter, "twitter_influencers.csv")
+
+# Get user info
+library(purrr)
+infl_twitter <- infl_twitter %>%
+  filter(influencer != "@dydxderivatives")
+
+user_data <- map_df(infl_twitter$influencer, ~ search_users(.x, n = 1))
